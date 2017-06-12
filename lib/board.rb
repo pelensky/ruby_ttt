8fab541837c1449c92814e910e3c4a8cdee1b3f4
Dan Pelensky <dan@pelensky.com>
2017-06-12 09:32:56 +0100

    can place marker

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,16 @@
 class Board
 
+  OFFSET = 1
+
   attr_reader :spaces
 
   def initialize(number_of_spaces)
     @number_of_spaces = number_of_spaces
     @spaces = (1..9).to_a
+  end
+
+  def place_marker(space, marker)
+    @spaces[space - OFFSET] = marker
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe Board do
     it "creates an array of spaces" do
       expect(board.spaces).to eq (1..9).to_a
     end
+  end
 
+  context "A user can" do
+
+    it "place a marker if the space is free" do
+      board.place_marker(1, "X")
+      expect(board.spaces[0]).to eq "X"
+    end
   end
 end
