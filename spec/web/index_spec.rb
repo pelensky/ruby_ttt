b4400c355cbf70a4551d93acd2c760038fce594b
Dan Pelensky <dan@pelensky.com>
2017-07-03 16:11:24 +0100

    tests for IDs for spaces

--- spec/web/index_spec.rb
+++ spec/web/index_spec.rb
@@ -2,5 +2,6 @@ feature "index" do
   scenario "main page" do
     visit '/'
     expect(current_path).to eq('/')
+    (0..8).each { |number| expect(page).to have_css("#" + number.to_s) }
   end
 end
