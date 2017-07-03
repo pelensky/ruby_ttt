c1e6d6cf56d61207a309367ef7332ed55d9acf16
Dan Pelensky <dan@pelensky.com>
2017-07-03 16:17:41 +0100

    fixed the test format

--- spec/web/index_spec.rb
+++ spec/web/index_spec.rb
@@ -1,7 +1,17 @@
 feature "index" do
-  scenario "main page" do
+  before :each do
     visit '/'
+  end
+
+  scenario "directs to the right page" do
     expect(current_path).to eq('/')
+  end
+
+  scenario "shows a title" do
+    expect(page).to have_content "Tic Tac Toe"
+  end
+
+  scenario "has all elements that will make up the tic tac toe board" do
     (0..8).each { |number| expect(page).to have_css("#" + number.to_s) }
   end
 end
