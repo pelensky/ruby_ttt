69f0be59d66f6c521db0865453d70094437f9a7e
Dan Pelensky <dan@pelensky.com>
2017-07-03 11:31:29 +0100

    simple computer spec updated for new board type

--- spec/simple_computer_spec.rb
+++ spec/simple_computer_spec.rb
@@ -7,14 +7,13 @@ RSpec.describe SimpleComputer do
 
     context "The computer" do
       it "selects the only available space" do
-        board = Board.new(["X","O","O","O","O","X","X","X",9])
-        expect(computer.choose_space(board)).to eq 9
+        board = Board.new(["X","O","O","O","O","X","X","X",nil])
+        expect(computer.choose_space(board)).to eq 8
       end
 
       it "selects a random available space" do
-        available_spaces = [1,2,3,4,5,6,7,8,9]
-        board = Board.new(available_spaces)
-        expect(available_spaces).to include computer.choose_space(board)
+        board = Board.new(Array.new(9, nil))
+        expect((0..8).to_a).to include computer.choose_space(board)
       end
     end
 
