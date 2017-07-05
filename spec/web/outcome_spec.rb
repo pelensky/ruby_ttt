a8016dc9539a9a84494e5f259f7ee5681fb49ab5
Dan Pelensky <dan@pelensky.com>
2017-07-05 16:37:30 +0100

    testing all three outcomes

---
+++ spec/web/outcome_spec.rb
@@ -0,0 +1,20 @@
+feature "outcome" do
+  before :each do
+    new_game_two_human
+  end
+
+  scenario "two humans, x wins" do
+    two_humans_x_wins
+    expect(page).to have_content "X is the winner"
+  end
+
+  scenario "two humanx, o wins" do
+    two_humans_o_wins
+    expect(page).to have_content "O is the winner"
+  end
+
+  scenario "two humans, tied game" do
+    two_humans_tied_game
+    expect(page).to have_content "Tied Game"
+  end
+end
